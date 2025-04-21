fb90f15a4d3902bdba865817635fc29f04067d67
Deepanshu Arya <whoami.arya@gmail.com>
2025-04-21 18:04:49 +0530

    rechecking twice

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
             className="text-xl font-bold cursor-pointer"
             onClick={() => scrollToSection("hero")}
           >
-            Dice-Med
+            DiceMed
           </div>
           <div className="hidden md:flex gap-8">
             {[
